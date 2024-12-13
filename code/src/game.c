a90124489f51b5904568633c124caf3dfae74d9d
vini464 <vini.dev2003@gmail.com>
2024-12-12 21:15:55 -0300

    projectile thread ready

--- code/src/game.c
+++ code/src/game.c
@@ -4,6 +4,14 @@
 #include "../include/gpu.h"
 #include "../include/sprite_api.h"
 
+#include <time.h>
+
+#define MAX_SHOTS 4
+#define SHOT_SPEED 4
+#define TOP 0
+#define BOTTOM 480
+#define LEFT 0
+#define RIGHT 640
 
 /*
  * Sobre os registradores:
@@ -19,8 +27,8 @@
 
 typedef struct player {
   Sprite sprite[2]; // cada jogador tem dois sprites 
-  Sprite shots[4]; // cada jogador tem no máximo 4 tiros;
-  int active_shots[4]; // usado para verificar se o tiro está acionado ou não
+  Sprite shots[MAX_SHOTS]; // cada jogador tem no máximo 4 tiros;
+  int active_shots[MAX_SHOTS]; // usado para verificar se o tiro está acionado ou não
   int avaiable_shots; // limite de tiros disponíveis
   int life;
   int damage;
@@ -50,20 +58,60 @@ int main(void) {
 }
 
 
+void timer(int trigger) {
+  int msec =0;
+  clock_t difference, before = clock();
+  do {
+    difference = clock() - before;
+    msec = difference * 1000 / CLOCKS_PER_SEC;
+  } while (msec < trigger);
+
+}
+
+
+void * projectilesThread(void * arg) {
+  int *stop = (int *)arg;
+  int i;
+  while (!(*stop)) {
+    for (i=0; i<MAX_SHOTS; i++) {
+      if (TANK.active_shots[i]){
+        TANK.shots[i].pos_y -= SHOT_SPEED;
+        if (checkCollision(&TANK.shots[i], &SHIP.sprite[0]) || checkCollision(&TANK.shots[i], &SHIP.sprite[0]) || TANK.shots[i].pos_y < TOP){
+          TANK.active_shots[i] = 0;
+          TANK.avaiable_shots++;
+          if (TANK.shots[i].pos_y >= TOP){
+            SHIP.life -= TANK.damage;
+          }
+        }
+      }
+      if (SHIP.active_shots[i]){
+        SHIP.shots[i].pos_y += SHOT_SPEED;
+        if (checkCollision(&SHIP.shots[i], &TANK.sprite[0]) || checkCollision(&SHIP.shots[i], &TANK.sprite[0]) || SHIP.shots[i].pos_y > BOTTOM - 20){ // TODO: alterar constante 20 para um valor variável dentro da sprite, fazer isso tambem com TOP 
+
+          SHIP.active_shots[i] = 0;
+          SHIP.avaiable_shots++;
+          if (SHIP.shots[i].pos_y <= BOTTOM - 20){
+            TANK.life -= SHIP.damage;
+          }
+        }
+      }
+    }
+    timer(4); // espera por 4 milisegundos
+  }
+  return NULL;
+}
+
+
 
 void * renderThread(void * arg) {
   int *stop = (int*) arg;
+  int i;
   while (!(*stop)) {
-    showSprite(&TANK.sprite[0]);
-    showSprite(&TANK.sprite[1]);
-    showSprite(&SHIP.sprite[0]);
-    showSprite(&SHIP.sprite[1]);
-    int i;
     for (i = 0; i < 2; i++) {
       showSprite(&TANK.sprite[i]);
       showSprite(&SHIP.sprite[i]);
     }
-    for (i = 0; i < 4; i++) {
+    for (i = 0; i < MAX_SHOTS; i++) {
       if (TANK.active_shots[i]) {
         showSprite(&TANK.shots[i]);
       }
