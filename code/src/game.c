16658f183f3a8a9cbc32b3ba53c148c927570705
vini464 <vini.dev2003@gmail.com>
2024-12-12 22:04:46 -0300

    player2 (ship) controller done

--- code/src/game.c
+++ code/src/game.c
@@ -68,6 +68,52 @@ void timer(int trigger) {
 
 }
 
+// Controle do jogador 2 (ship)
+void * mouseThread(void * arg) {
+  int * stop = (int *) arg;
+  int fd = open_mouse();
+  int i; // auxiliar para percorrer os sprites
+  mouse_event mouse ={0};
+  while (!(*stop)) {
+    read_mouse(fd, &mouse);
+    if (mouse.btn_left) {
+      if (SHIP.avaiable_shots > 0) {
+        for (i=0; i<MAX_SHOTS; i++) {
+          if (!SHIP.active_shots[i]) {
+            SHIP.active_shots[i] = 1;
+            SHIP.shots[i].pos_x = SHIP.sprite[1].pos_x;
+            SHIP.shots[i].pos_y = SHIP.sprite[0].pos_y;
+            SHIP.shots[i].act = 1;
+            SHIP.shots[i].reg_id = i+11; // registrador fixo
+            SHIP.shots[i].mem_offset = 11; // TODO: atualizar este valor
+            break;
+          }
+        }
+      }
+      mouse.btn_left = 0;
+    }
+    for (i=0; i<2; i++) {
+      SHIP.sprite[i].pos_x += mouse.direction.x;
+      if (checkCollision(&SHIP.sprite[i], &TANK.sprite[0]) || checkCollision(&SHIP.sprite[i], &TANK.sprite[1])){
+        SHIP.sprite[i].pos_x -= mouse.direction.x;
+      }
+      SHIP.sprite[i].pos_y -= mouse.direction.y;
+      if (checkCollision(&SHIP.sprite[i], &TANK.sprite[0]) || checkCollision(&SHIP.sprite[i], &TANK.sprite[1])){
+        SHIP.sprite[i].pos_y += mouse.direction.y;
+      }
+      if (SHIP.sprite[i].pos_y < TOP) 
+        SHIP.sprite[i].pos_y = TOP;
+      else if (SHIP.sprite[i].pos_y > BOTTOM - 20) // TODO: trocar 20 por tamanho do sprite
+        SHIP.sprite[i].pos_y = TOP;
+      else if (SHIP.sprite[i].pos_x < LEFT + i *20)
+        SHIP.sprite[i].pos_x = LEFT + i*20;
+      else if (SHIP.sprite[i].pos_x > RIGHT - (20 * i) + 40)
+        SHIP.sprite[i].pos_x = RIGHT + RIGHT - (20 * i) + 40;
+    }
+  }
+  close_mouse(fd);
+  return NULL;
+}
 
 void * projectilesThread(void * arg) {
   int *stop = (int *)arg;
